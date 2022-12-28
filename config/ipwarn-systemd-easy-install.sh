18cb02893c29f4042ece48d435e8601b16f13af9
Pablo Gómez-Caldito <pablo@streamsets.com>
2022-12-28 14:36:24 +0100

    Easy install script

---
+++ config/ipwarn-systemd-easy-install.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+
+if [ $EUID > 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+
+# Download files (program, config and service)
+echo "Downloading files..."
+mkdir /tmp/ipwarn-install
+pushd /tmp/ipwarn-install
+curl -O https://raw.githubusercontent.com/caldito/ipwarn/master/ipwarn
+curl -O https://raw.githubusercontent.com/caldito/ipwarn/master/config/ipwarn.conf
+curl -O https://raw.githubusercontent.com/caldito/ipwarn/master/config/ipwarn.service
+echo "Files downloaded successfully..."
+
+# Create user
+echo "Creating user..."
+useradd --system ipwarn
+echo "User created successfully"
+
+# Copy files and set permissions
+echo "Copying files and setting permissions"
+cp ./ipwarn /usr/local/bin/ipwarn
+chown ipwarn:ipwarn /usr/local/bin/ipwarn
+chmod 755 /usr/local/bin/ipwarn
+
+mkdir /etc/ipwarn
+chown ipwarn:ipwarn /etc/ipwarn
+chmod 755 /etc/ipwarn
+
+cp ./ipwarn.conf /etc/ipwarn/ipwarn.conf
+chown ipwarn:ipwarn /etc/ipwarn/ipwarn.conf
+chmod 600 /etc/ipwarn/ipwarn.conf
+
+cp ./ipwarn.service /etc/systemd/system/ipwarn.service
+chown root /etc/systemd/system/ipwarn.service
+chmod 644 /etc/systemd/system/ipwarn.service
+popd
+echo "Files copied and permissions set successfully"
+
+# Prepare systemd service
+echo "Preparing systemd service..."
+systemctl reload-daemon
+systemctl start ipwarn.service
+echo "Systemd service prepared successfully\n"
+
+# Finish
+echo "ipwarn installation completed successfully!\n"
+
+echo "The program is now running with the default config. You can edit the config in \"/etc/ipwarn/ipwarn.conf\" and then restart the ipwarn service runinng \"systemctl restart ipwarn\""
+echo "If you wish the service to run on startup run: systemctl enable ipwarn.service"
